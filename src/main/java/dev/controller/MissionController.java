5ab6ee284932727fb8a725b398898623888e27ee
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-08-22 19:10:06 +0200

    fonctionnalité demande mission

--- src/main/java/dev/controller/MissionController.java
+++ src/main/java/dev/controller/MissionController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.domain.Mission;
 import dev.repository.MissionRepo;
+import dev.service.MissionService;
 
 @CrossOrigin(allowCredentials = "true")
 @RestController
@@ -19,6 +21,9 @@ public class MissionController {
 
     @Autowired
     private MissionRepo missionRepo;
+
+    @Autowired
+    private MissionService missionService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/missions")
     public List<Mission> getMissions(@RequestParam Long id) {
@@ -30,6 +35,11 @@ public class MissionController {
     @RequestMapping(method = RequestMethod.DELETE, path = "/missions")
     public void deleteMission(@RequestParam Long id) {
         this.missionRepo.deleteById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/missions")
+    public Mission createMission(@RequestBody Mission mission) {
+        return this.missionService.createMission(mission);
     }
 
 }
---
+++ src/main/java/dev/controller/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package dev.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import dev.exception.MissionInvalideException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = MissionInvalideException.class)
+    protected ResponseEntity<Object> handleConflict(MissionInvalideException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Mission invalide !" + ex.getMessage());
+    }
+
+}
