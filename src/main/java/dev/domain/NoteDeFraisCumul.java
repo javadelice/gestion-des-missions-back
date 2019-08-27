4c04fd6ae8e433e61b988d2808e924250715d1d9
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-08-27 17:40:12 +0200

    issue-10 : planning missions OK

--- src/main/java/dev/domain/NoteDeFraisCumul.java
+++ src/main/java/dev/domain/NoteDeFraisCumul.java
@@ -11,71 +11,63 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class NoteDeFraisCumul {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@OneToMany(mappedBy = "ndfCumul")
-	private List<NoteDeFrais> notesDeFrais;
-	
-	@OneToOne
-	@JoinColumn(name = "mission_id")
-	 private Mission mission;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	
+    @OneToMany(mappedBy = "ndfCumul")
+    @JsonIgnore
+    private List<NoteDeFrais> notesDeFrais;
 
-	public NoteDeFraisCumul() {
-		super();
-		this.notesDeFrais = new ArrayList<NoteDeFrais>();
-		
-	}
+    @OneToOne
+    @JoinColumn(name = "mission_id")
+    @JsonIgnore
+    private Mission mission;
 
-	
-	
-	
-	public NoteDeFraisCumul(List<NoteDeFrais> notesDeFrais, Mission mission) {
-		super();
+    public NoteDeFraisCumul() {
+        super();
+        this.notesDeFrais = new ArrayList<NoteDeFrais>();
+    }
 
-		this.notesDeFrais = notesDeFrais;
-		this.mission = mission;
-	}
+    public NoteDeFraisCumul(List<NoteDeFrais> notesDeFrais, Mission mission) {
+        super();
 
+        this.notesDeFrais = notesDeFrais;
+        this.mission = mission;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public List<NoteDeFrais> getNotesDeFrais() {
+        return notesDeFrais;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setNotesDeFrais(List<NoteDeFrais> notesDeFrais) {
+        this.notesDeFrais = notesDeFrais;
+    }
 
-	public List<NoteDeFrais> getNotesDeFrais() {
-		return notesDeFrais;
-	}
+    public void addNotesDeFrais(NoteDeFrais noteDeFrais) {
+        this.notesDeFrais.add(noteDeFrais);
+    }
 
-	public void setNotesDeFrais(List<NoteDeFrais> notesDeFrais) {
-		this.notesDeFrais = notesDeFrais;
-	}
-	
-	public void addNotesDeFrais(NoteDeFrais noteDeFrais){
-		this.notesDeFrais.add(noteDeFrais);
-	}
+    public Mission getMission() {
+        return mission;
+    }
 
-	public Mission getMission() {
-		return mission;
-	}
+    public void setMission(Mission mission) {
+        mission.setNdfCumul(this); // à améliorer
+        this.mission = mission;
+    }
 
-	public void setMission(Mission mission) {
-		mission.setNdfCumul(this); // à améliorer
-		this.mission = mission;
-	}
-	
-	
-	
-	
 }
