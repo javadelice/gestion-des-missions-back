c2b43d6127a586348207aab7a9a4a913d5ea776f
Clement BRIEUC <clementbrieuc@gmail.com>
2019-08-29 17:55:29 +0200

    affichage fenetre de modification

--- src/main/java/dev/controller/NatureController.java
+++ src/main/java/dev/controller/NatureController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.domain.Nature;
 import dev.repository.NatureRepo;
+import dev.service.NatureService;
 
 @CrossOrigin(allowCredentials = "true")
 @RestController
@@ -19,6 +21,9 @@ public class NatureController {
 
     @Autowired
     private NatureRepo natureRepo;
+    
+    @Autowired
+    private NatureService natureService;
 
     // Accès à la page "nature" qu'à ceux ayant le rôle d'administrateur
     @Secured("ROLE_ADMINISTRATEUR")
@@ -32,4 +37,8 @@ public class NatureController {
         return this.natureRepo.findAll();
     }
 
-}
+    @RequestMapping(method = RequestMethod.PATCH, path = "/nature")
+    public Nature modifyNature(@RequestBody Nature nature) {
+            return this.natureService.createNature(nature);
+        }
+    }
