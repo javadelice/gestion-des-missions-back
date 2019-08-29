1df8144c373a9fb54cbbd41d2649e3e17a2f31e8
Glen OLLIVIER <glen.ollivier@gmail.com>
2019-08-29 17:33:26 +0200

    restauration de la configuration web de spring security

--- src/main/java/dev/security/WebSecurityConfig.java
+++ src/main/java/dev/security/WebSecurityConfig.java
@@ -23,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${jwt.cookie}")
